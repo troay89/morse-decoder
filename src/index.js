c1045a4c23ab8ded6d1a21fb3f2dc428742357dc
Home <picccc89@gmail.com>
2023-01-06 20:28:30 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let binaryArm = lettersBinary(expr)
+    let morseArr = binaryToMorse(binaryArm)
+    return morseToWord(morseArr)
 }
 
 module.exports = {
     decode
+}
+
+function lettersBinary(expr) {
+    let letters = [];
+    let count = 0; 
+    do {
+        let letter = expr.substr(count, 10)
+        count += 10
+        letters.push(letter)
+    } while (count < expr.length)
+    return letters
+}
+
+function binaryToMorse(arr) {
+    let arrBinary = [];
+    for (let binary of arr) {
+        let withoutEleven = binary.replace(new RegExp('10', 'g'), '.')
+        let withoutTen = withoutEleven.replace(new RegExp('11', 'g'), '-')
+        let withoutZero = withoutTen.replace(new RegExp('00', 'g'), '')
+        arrBinary.push(withoutZero)
+    }
+    return arrBinary;
+}
+
+function morseToWord(arr) {
+    let centence = '';
+    for (let morse of arr) {
+        if (morse !== '**********') {
+            centence += MORSE_TABLE[morse]
+        } else centence += ' '
+    }
+    return centence;
 }
